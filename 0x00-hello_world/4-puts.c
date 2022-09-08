842c8d7b49de75497092ca1c8bbc66b0f8e913b2
Bwiso <bwiso.daka@gmail.com>
2022-09-08 21:42:25 +0200

    my new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-/* Code to print a message main.c*/
-
+/* Code to print a message */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+		eturn (0);
 }
