4985df23215e3d4cb1a4ad791019aa6cc7712256
Bwiso <bwiso.daka@gmail.com>
2022-09-15 15:26:04 +0200

    my commit task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,13 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - function to print abc 10 times
- *
- * Return: 0
- */
-void print_alphabet_x10(void)
-{
-	char c, i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-	}
+/**
+*mul - multiplies two numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
 }
