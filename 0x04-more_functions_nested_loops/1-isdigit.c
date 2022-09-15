ddcf85072a7e2a6928f5ea37ec55fe41d450056b
Bwiso <bwiso.daka@gmail.com>
2022-09-15 13:35:33 +0200

    my commit task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,13 +6,10 @@
  *
  * @c: int to be checked
  *
- * Return: 1 if c is a digit, otherwise 0
+ * Return: success
  **/
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+	return (c >'0'&& c <= '9');
 }
